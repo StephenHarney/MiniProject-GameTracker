a231bf2298e2def245bca57b59b0c63bf14c5d2a
Stephen Harney <g00367573@atu.ie>
2023-12-06 20:12:43 +0000

    Initial commit

---
+++ src/main/java/ie/atu/hello/MiniProjectGameTrackerApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectGameTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjectGameTrackerApplication.class, args);
+	}
+
+}
